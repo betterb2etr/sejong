628b65f789fbb5c6527c8fa69d5737ff3e522199
b2etr <102886338+betterb2etr@users.noreply.github.com>
2024-12-26 17:25:12 +0900

    fix : timer start button
    timer start button

--- my-project/src/views/TimerView.vue
+++ my-project/src/views/TimerView.vue
@@ -3,6 +3,7 @@
     <h2>Timer</h2>
     <p>120분 화이팅입니다!</p>
     <TimerCPT :initialTime="7200" ref="timer" />
+    <button v-if="!timerStarted" @click="startTimer" class="start-button">시작</button>
   </div>
 </template>
 
@@ -14,8 +15,16 @@ export default {
   components: {
     TimerCPT,
   },
-  mounted() {
-    this.$refs.timer.startTimer(); // 타이머 시작
+  data() {
+    return {
+      timerStarted: false,
+    };
+  },
+  methods: {
+    startTimer() {
+      this.timerStarted = true;
+      this.$refs.timer.startTimer(); // 타이머 시작
+    },
   },
 };
 </script>
@@ -28,5 +37,19 @@ export default {
 
 h2 {
   color: #FF9800;
+}
+
+.start-button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+.start-button:hover {
+  background-color: #45a049;
 }
 </style>
