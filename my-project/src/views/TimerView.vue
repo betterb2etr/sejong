1ecdabaa0f18ba966f29f8090963778088439f3e
b2etr <102886338+betterb2etr@users.noreply.github.com>
2024-12-26 17:20:37 +0900

    fix : timer revival
    fix : timer revival

--- my-project/src/views/TimerView.vue
+++ my-project/src/views/TimerView.vue
@@ -1,23 +1,31 @@
 <template>
-  <div>
+  <div class="timer-view">
     <h2>Timer</h2>
     <p>120분 화이팅입니다!</p>
-    <TimerCPT />
+    <TimerCPT :initialTime="7200" ref="timer" />
   </div>
 </template>
 
 <script>
-import TimerCPT from '@/components/TimerCPT.vue';
+import TimerCPT from "@/components/TimerCPT.vue";
 
 export default {
-  name: 'TimerView',
+  name: "TimerView",
   components: {
     TimerCPT,
+  },
+  mounted() {
+    this.$refs.timer.startTimer(); // 타이머 시작
   },
 };
 </script>
 
-<style>
+<style scoped>
+.timer-view {
+  text-align: center;
+  margin-top: 20px;
+}
+
 h2 {
   color: #FF9800;
 }
